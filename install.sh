070ce96b55a7877de7c7713bf6814866f4c0b4f5
Scott Griepentrog <sgriepentrog@digium.com>
2014-04-29 15:40:58 -0500

    first test scripts

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# install components needed to operate automaton scripts
+set -exu
+
+[ ! -d cron ] && echo ERROR: missing cron directory && exit 1
+
+CRONS=($(ls -1 cron))
+for CRON in ${CRONS[*]}
+do
+	if [ ! -L /etc/cron.$CRON/automaton ]
+	then
+		sudo ln -s $PWD/cron/$CRON /etc/cron.$CRON/automaton
+	fi
+done
+
